f1e999d549be6c9e38baff1ddd36379497e7b6f5
Majak Deng <majakbanks.deng>
2024-10-29 17:10:57 +0300

    modified

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,20 @@
 class Course:
-    pass
+    def __init__(self, title, schedule, description):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+
+    def get_title(self):
+        return self.title
+
+    def get_schedule(self):
+        return self.schedule
+
+    def get_description(self):
+        return self.description
+
+    def __str__(self):
+        output = ''
+        output += f'Title: {self.title}\nSchedule: {self.schedule}\nDescription: {self.description}\n'
+        output += '------------------'
+        return output
